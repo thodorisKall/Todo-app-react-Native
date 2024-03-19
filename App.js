22f7d30c277b79f69ac85d466af1f54cf8957fe9
thodorisKall <105176440+thodorisKall@users.noreply.github.com>
2024-03-20 01:03:14 +0200

    docs: initialize project and the starting screen

--- App.js
+++ App.js
@@ -1,20 +1,47 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, SafeAreaView } from "react-native"
+import { useState } from "react"
+import Todo from "./components/todo"
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    <SafeAreaView style={styles.container}>
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Todo list</Text>
+      </View>
+      <View style={styles.test}>
+        <Todo />
+        <Todo />
+        <Todo />
+        <Todo />
+      </View>
+    </SafeAreaView>
+  )
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "top",
+    backgroundColor: "#242124",
   },
-});
+  headerContainer: {
+    backgroundColor: "#5f9ea0",
+    paddingTop: 45,
+    width: "100%",
+    height: "12%",
+    textAlign: "center",
+  },
+  header: {
+    textAlign: "center",
+    fontSize: 32,
+    fontWeight: "bold",
+  },
+  test: {
+    marginTop: 30,
+    alignItems: "center",
+    gap: 14,
+    width: "100%",
+  },
+})
